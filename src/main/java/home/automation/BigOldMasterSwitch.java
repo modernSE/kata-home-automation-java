c1d297c27ae71cb40ad2c702fc297d0b174fba25
niklasabc <niklas.baumgaertner@cas.de>
2022-06-13 14:29:29 +0000

    Refactor

--- src/main/java/home/automation/BigOldMasterSwitch.java
+++ src/main/java/home/automation/BigOldMasterSwitch.java
@@ -1,5 +1,7 @@
 package home.automation;
 
+import java.util.List;
+
 /**
  * Created by Ferdinand.Szekeresch on 20.04.2017.
  */
@@ -17,14 +19,16 @@ public class BigOldMasterSwitch {
 
 	private CoffeeMaker coffeeMaker = new CoffeeMaker();
 
+	private List<ISmartDevice> deviceList = List.of(new Shutter());
+
+
+
 	public void press() {
 		if (!isOn) {
-			System.out.println("BIG OLD SWITCH PRESSED.\n\n");
-			shutter.close();
-			airConditioning.setTemperatureInCelsius(20);
-			lights.dimPercent(50);
-			stereo.play("Bob Marley");
-			coffeeMaker.brew(CoffeeMaker.Type.DECAF);
+		for (ISmartDevice iSmartDevice : deviceList) {
+			iSmartDevice.switchState();
+		}
+		
 			isOn = true;
 			StringBuffer b = new StringBuffer();
 			b.append("         |\n");
@@ -39,14 +43,8 @@ public class BigOldMasterSwitch {
 			b.append("       |___|\n");
 			System.out.println(b.toString());
 		} else if (isOn) {
-			shutter.open();
-			airConditioning.turnOff();
-			lights.off();
-			stereo.rememberPosition();
-			stereo.off();
-			if (coffeeMaker.isOn()) {
-				coffeeMaker.doClean();
-				coffeeMaker.shutDown();
+			for (ISmartDevice iSmartDevice : deviceList) {
+				iSmartDevice.switchState();
 			}
 		}
 	}
